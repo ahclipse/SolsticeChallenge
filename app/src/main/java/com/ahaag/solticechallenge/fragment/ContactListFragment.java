1183aaef063f2f1a4000dbd13cf81fe855d6c821
Adam <ahclipse@gmail.com>
2017-05-07 18:46:24 -0500

    Recycler View framework setup

--- app/src/main/java/com/ahaag/solticechallenge/fragment/ContactListFragment.java
+++ app/src/main/java/com/ahaag/solticechallenge/fragment/ContactListFragment.java
@@ -1,8 +1,28 @@
 package com.ahaag.solticechallenge.fragment;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ahaag.solticechallenge.R;
 
 public class ContactListFragment extends Fragment {
 
+    private RecyclerView mContactRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_contact_list, container, false);
+        mContactRecyclerView = (RecyclerView) v.findViewById(R.id.contact_recycler_view);
+        mContactRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
+    }
 }
